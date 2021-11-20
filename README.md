210d401b63f53e4ca99602ea42624e10a9f97a5d
Vadim Veksler <reyzele91@gmail.com>
2021-11-20 21:42:28 +0200

    gh pages

--- README.md
+++
@@ -1,6 +0,0 @@
-## FB Friends filter with mock data
-
-[Preview](https://reyzele.github.io/fb-friends-filter/)
-
-* yarn / npm install
-* yarn start / npm start --> will run gulp
--- src/scripts/main.js
+++
@@ -1,8 +0,0 @@
-import InitApp from "./common/auth";
-import Dnd from "./common/dnd";
-import Filter from "./common/filter";
-
-const params = InitApp();
-const filterMap = Filter(params);
-
-Dnd(params, filterMap);
